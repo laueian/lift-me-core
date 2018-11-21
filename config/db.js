5a38227fccffb17abc7f2be6a9a9ca2d2a6e5331
shahedsalehian <shahed.salehian.805@my.csun.edu>
2018-11-20 20:18:19 -0800

    removed creds. pls forgive lol

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //Do not commit creds to repo please
-  url: "mongodb://lift-me_admin:comp484-SIDS@ds037997.mlab.com:37997/lift-me"
+  url: "mongodb://user:pass@ds037997.mlab.com:37997/lift-me"
 };
