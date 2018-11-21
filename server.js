78668a1955890f6e420fd1fb4e7ab8f6989edc57
shahedsalehian <shahed.salehian.805@my.csun.edu>
2018-11-20 20:15:47 -0800

    removed bodyparser

--- server.js
+++ server.js
@@ -2,10 +2,6 @@
 const express = require("express");
 const app = express();
 
-// bodyParse to conveniently get paramaters out of urls
-const bodyParser = require("body-parser");
-// app.use(bodyParser.urlencoded({ extended: true }));
-
 // Mongoose Connection Setup
 const mongoose = require("mongoose");
 const db = require("./config/db");
@@ -17,9 +13,6 @@ connection.on('error', console.error.bind(console, "MongoDB connection error:"))
 
 // set responses as JSON
 app.set('json spaces', 40);
-
-// Include the quote model
-const Quote = require("./models/quote");
 
 // Include routes
 const quoteRoutes = require("./routes/quotes");
