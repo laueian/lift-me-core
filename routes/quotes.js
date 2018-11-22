a001fd80ba7d96092b8c86de3f3da0aeaab70382
Ian Laue <laueian@gmail.com>
2018-11-22 02:04:41 -0800

    Body Parser and CRUD using Mongoose helper methods for Quote Collection

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,15 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Quotes = require('../models/quote.js');
+const Quotes = require("../models/quote.js");
 
 router.get("/quotes", (req, res) => {
-    Quotes.find({}, (err, quotes) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(quotes);
-        }
-    });
-})
+  Quotes.find({}, (err, quotes) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(quotes);
+    }
+  });
+});
+
+router.post("/quotes", function(req, res) {
+  const quote = new Quotes(req.body);
+  quote.save(err => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(quote);
+  });
+});
+
+router.put("/quotes/:id", function(req, res) {
+  Quotes.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, quote) => {
+      if (err) return res.status(500).send(err);
+      return res.status(200).send(quote);
+    }
+  );
+});
+
+router.delete("/quotes/:id", function(req, res) {
+  Quotes.findByIdAndRemove(req.params.id, (err, quote) => {
+    if (err) return es.status(500).send(err);
+    const response = {
+      message: "Quote successfully deleted",
+      id: quote._id
+    };
+    return res.status(200).send(response);
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 //Express Setup
 const express = require("express");
 const app = express();
+// Body Parser - Middleware for handling JSON boodies
+const bodyParser = require("body-parser");
+
+//Body Parser Setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Mongoose Connection Setup
 const mongoose = require("mongoose");
 const db = require("./config/db");
 const mongoDB = db.url;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.connect(
+  mongoDB,
+  { useNewUrlParser: true }
+);
 mongoose.Promise = global.Promise;
 let connection = mongoose.connection;
-connection.on('error', console.error.bind(console, "MongoDB connection error:"));
+connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
 // set responses as JSON
-app.set('json spaces', 40);
+app.set("json spaces", 40);
 
 // Include routes
 const quoteRoutes = require("./routes/quotes");
 
 // Defining the port on which the server is going to run
-const port = (process.env.PORT || 8000);
+const port = process.env.PORT || 8000;
 
 // include routes
 app.use("/", quoteRoutes);
@@ -26,4 +38,4 @@ app.use("/", quoteRoutes);
 // Server is started and is listening on the defined port
 app.listen(port, () => {
   console.log("We're live on " + port);
-})
+});
