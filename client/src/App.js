7ff266b4454449d594035abf027234872c4268f2
shahedsalehian <shahed.salehian.805@my.csun.edu>
2018-12-03 12:24:51 -0800

    added react front-end to application with link to express api

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import Card from './components/Card.js';
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      quotes: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/quotes").then((response) => {
+      return response.json();
+    }).then((data)=>{
+      this.setState({quotes: data});
+    });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            {this.state.quotes.map((item,i) => {
+              return <Card body={item.body} author={item.author} key={i} />
+            })}
+          </ul>    
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
