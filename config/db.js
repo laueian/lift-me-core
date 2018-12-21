1103ebae65a14f5459c964071a889b186d1e8688
laueian <laueian@gmail.com>
2018-12-21 13:05:50 -0800

    attempt 2

--- config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: "mongodb://admin_lift-me:SanDisk512!@ds037997.mlab.com:37997/lift-me"
-};
--- config/scraper.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  url:
-    "https://7p7rvvekjb.execute-api.us-west-1.amazonaws.com/default/motivational-scraping?url=",
-  targetWebsiteUrl: "https://www.brainyquote.com"
-};
